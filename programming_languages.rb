fdebd22f23b926d6f2b55752d75c637285d672be
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-27 05:11:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,21 @@
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |key, value|
-
+  languages.each do |language_details, value|
+    language.each do |language, attributes|
+      attributes.each do |attribute, attribute_value|
+        if language == :javascript
+          new_hash[language] = {
+            :type => attribute_value,
+            :style => [:oo, :functional]
+          }
+        else
+          new_hash[language] = {
+            :type => attribute_value,
+            :style => [language_details]
+          }
+        end
+      end
+    end
   end
-
   new_hash
 end
